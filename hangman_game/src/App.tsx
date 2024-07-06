059f97d974dbc3947e99d18ca803ed20748ce43f
Sadikshya Satyal <sadikshyasatyal50@gmail.com>
2024-07-06 21:12:36 +0300

    construction of the hangman lines

--- hangman_game/src/App.tsx
+++ hangman_game/src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
 
 
 function App() {
